529971be80f8b3223151d796d8cb29b3a65dbc58
3846masa <3846masahiro+git@gmail.com>
2015-11-15 12:48:30 +0900

    Add Cluster

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import sourceMap from 'source-map-support';
+sourceMap.install();
+
+import Express from 'express';
+import bodyParser from 'body-parser';
+import methodOverride from 'method-override';
+import passportRouter from './passport-router.js';
+import mongoRouter from './mongo-router.js';
+import rpgmvProxyRouter from './rpgmv-save-store-proxy.js';
+import { path as APP_ROOT_PATH } from 'app-root-path';
+import path from 'path';
+
+let app = Express();
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+app.use(passportRouter);
+app.use(mongoRouter);
+app.use(rpgmvProxyRouter);
+app.use(Express.static(path.resolve(APP_ROOT_PATH, 'static')));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send(`${err.name}: ${err.message}`);
+});
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,13 @@
-import sourceMap from 'source-map-support';
-sourceMap.install();
+import cluster from 'cluster';
+import app from './app';
+import { cpus } from 'os';
+const CPU_NUM = cpus().length;
+const PORT = process.env.PORT || 3000;
 
-import Express from 'express';
-import bodyParser from 'body-parser';
-import methodOverride from 'method-override';
-import passportRouter from './passport-router.js';
-import mongoRouter from './mongo-router.js';
-import rpgmvProxyRouter from './rpgmv-save-store-proxy.js';
-import { path as APP_ROOT_PATH } from 'app-root-path';
-import path from 'path';
-
-let app = Express();
-
-app.set('PORT', process.env.PORT || 3000);
-
-app.use(bodyParser.json());
-app.use(methodOverride());
-
-app.use(passportRouter);
-app.use(mongoRouter);
-app.use(rpgmvProxyRouter);
-app.use(Express.static(path.resolve(APP_ROOT_PATH, 'static')));
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send(`${err.name}: ${err.message}`);
-});
-app.listen(app.get('PORT'), '0.0.0.0', () => {
-  console.log('Express server listening on port ' + app.get('PORT'));
-});
+if (cluster.isMaster) {
+  for (var i = 0; i < CPU_NUM; i++) {
+    cluster.fork();
+  }
+} else {
+  app.listen(PORT, '0.0.0.0', () => {});
+}
